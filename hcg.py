24fbb25e8dafd70e5d8cb093d49a69593573bfb1
Ale <alecominotti@gmail.com>
2020-09-04 23:33:43 -0300

    more parameters and fixed localhost

--- hcg.py
+++ hcg.py
@@ -39,8 +39,8 @@ else:
 
 
 # User Variables -----
-hideBrowserMediaPopUps = True # True uf you want to remove microphone and camera permission pop ups. False if you want to use them
-closeBrowserAtExit = True  # True: keeps browser open when script is stopped. False: otherwise
+hideBrowserMediaPopUps = True # True if you want to remove microphone and camera permission pop ups. False if you want to use them
+keepBrowserOpenAtExit = True  # True: keeps browser open when script is stopped. False: otherwise. NOT WORKING ON WINDOWS
 minRandomFunctions = 0 #lower bound value for functions amount
 maxRandomFunctions = 5 #upper bound value for functions amount
 minRandomArgument = 0  #lower bound value for the source and function arguments
@@ -68,7 +68,8 @@ BOLD = '\033[1m'
 ITALIC = '\033[3m'
 WHITE = '\033[0m'   
 resourcesFolder="resources/"
-txtfile="hydraCode.txt"
+txtfile="hydraCodes.txt"
+terminalSize = os.get_terminal_size().columns
 web=False
 if(runningOnMac):
     controlKey=Keys.COMMAND
@@ -77,23 +78,33 @@ else:
 # End Script Variables -----
 
 
-def printBanner():
+def printBanner(terminalSize):
     if(runningOnWindows):
         os.system("cls")
     else:
         os.system("clear")
-    print(YELLOW + """  _   _           _              ____          _         ____                           _             
+    if(terminalSize>102):
+        print(YELLOW + """  _   _           _              ____          _         ____                           _             
  | | | |_   _  __| |_ __ __ _   / ___|___   __| | ___   / ___| ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
  | |_| | | | |/ _` | '__/ _` | | |   / _ \ / _` |/ _ \ | |  _ / _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
  |  _  | |_| | (_| | | | (_| | | |__| (_) | (_| |  __/ | |_| |  __/ | | |  __/ | | (_| | || (_) | |   
  |_| |_|\__, |\__,_|_|  \__,_|  \____\___/ \__,_|\___|  \____|\___|_| |_|\___|_|  \__,_|\__\___/|_|   
         |___/
                 """ + WHITE)
-
+    else:
+        print(YELLOW + """  _   _    ____    ____ 
+ | | | |  / ___|  / ___|
+ | |_| | | |     | |  _ 
+ |  _  | | |___  | |_| |
+ |_| |_|  \____|  \____|
+                        """ + WHITE)
 def showInfo():
-    printBanner()
+    printBanner(terminalSize)
     helptxt=open(resourcesFolder + "help.txt", 'r').read()
     print(helptxt)
+
+def printError(message):
+    print(RED+"ERROR: " + WHITE + message)
 
 
 #Argument handling -----
@@ -103,8 +114,7 @@ parser.add_argument("-fmin", type=int, metavar='<Integer>', help="Minimum amount
 parser.add_argument("-fmax", type=int, metavar='<Integer>', help="Maximum amount of functions")
 parser.add_argument("-amin", type=float, metavar='<Float>', help="Minimum value for arguments")
 parser.add_argument("-amax", type=float, metavar='<Float>', help="Maximum value for arguments")
-parser.add_argument("-web", action='store_true', help="Open hydra in web browser with the generated code")
-parser.add_argument("-info", "--info", action='store_true', help="Shows information")
+parser.add_argument("-info", "--info", action='store_true', help="Shows longer help and information")
 parser.add_argument("-i", "--ignore", type=str, metavar='<source1,source2,func1,func2,...sourceN,funcN>', help="Specify which sources or functions to ignore. (ex.: osc,brightness)")
 parser.add_argument("--use-all", action='store_true', help="Doesn't ignore any source or function.")
 parser.add_argument("-xs", type=str, metavar='<source1,source2,...sourceN>', help="Specify exclusive sources to use. (ex.: osc,voronoi)")
@@ -112,8 +122,12 @@ parser.add_argument("-xf", type=str, metavar='<func1,func2,...funcN>', help="Spe
 parser.add_argument("-ap", "--arrow-prob", type=int, metavar='<Integer>', help="Probability of generating an arrow function as an argument.")
 parser.add_argument("-mp", "--mouse-prob", type=int, metavar='<Integer>', help="Probability of generating a mouse arrow function as an argument.")
 parser.add_argument("-mip", "--modulate-itself-prob", metavar='<Integer>', type=int, help='Probability of generating an modulation function with "o0" as an argument.')
+parser.add_argument("-web", action='store_true', help="Open hydra in web browser with the generated code")
 parser.add_argument("-live", action='store_true', help="Starts a live session where HCG writes automatically to Hydra in the web browser.")
-parser.add_argument("-l", "--localhost", type=int, metavar='<PORT>', help="Allows you to use your locally running Hydra")
+parser.add_argument("-hc", "--hide-code", action='store_true', help="Hides executed code when running a Live Session.")
+parser.add_argument("-um", "--use-media", action='store_true', help="Allows camera and microphone in Live Session Mode.")
+parser.add_argument("-cb", "--close-browser", action='store_true', help="Closes browser in Live Session Mode when script is stopped. NOT COMPATIBLE WITH WINDOWS")
+parser.add_argument("-l", "--localhost", type=str, metavar='<PORT|IP>', help="Allows you to use your locally running Hydra")
 args = parser.parse_args()
 
 if args.info:
@@ -130,7 +144,7 @@ else:
     if args.fmax or args.fmax==0:
         maxRandomFunctions=args.fmax
     if args.fmin and args.fmax and (args.fmin>args.fmax):
-        print(RED+"ERROR: " + WHITE + "Function max value must be bigger than min value.")
+        printError("Function max value must be bigger than min value.")
         exit(1)
 if args.ignore:
     ignoredList = args.ignore.split(",")
@@ -153,7 +167,7 @@ if args.use_all:
     exclusiveSourceList = []
     exclusiveFunctionList = []
 if args.web and args.live:
-    print(RED+"ERROR: " + WHITE + "Can't use Web and Live Session mode at the same time.")
+    printError("Can't use Web and Live Session mode at the same time.")
     exit(1)
 if args.localhost or args.localhost==0:
     hydraURL="https://localhost:" + str(args.localhost) + "/?code="
@@ -161,80 +175,100 @@ else:
     hydraURL="https://hydra.ojack.xyz/?code="
 if args.web:
     web=True
+hideCode=args.hide_code
+hideBrowserMediaPopUps = not args.use_media
+keepBrowserOpenAtExit = not args.close_browser  
+
 #End argument handling -----
 
 def setWebDriver():
-    warnings.simplefilter("ignore", category=DeprecationWarning) # just because I'm using chromedriver from hcg folder
+    warnings.simplefilter("ignore", category=DeprecationWarning) # just because I'm using the chromedriver inside hcg folder, not PATH
     opt = Options()
     if hideBrowserMediaPopUps:
         opt.add_argument("--use-fake-ui-for-media-stream")
-    opt.add_experimental_option("detach", closeBrowserAtExit)
+    opt.add_experimental_option("detach", keepBrowserOpenAtExit)
     caps = webdriver.DesiredCapabilities.CHROME.copy()
     caps['acceptInsecureCerts'] = True
+    webDriversRootFolder = "resources/webdrivers/"
     if runningOnLinux:
-        driverpath=r"resources/chromedriver"
+        driverpath= webDriversRootFolder + "linux/chromedriver"
     elif runningOnMac:
-        driverpath=r"path/to/mac/driver"
+        driverpath= webDriversRootFolder + "mac/chromedriver"
     elif runningOnWindows:
-        driverpath=r"resources/chromedriver.exe"
+        driverpath= webDriversRootFolder + "windows/chromedriver"
     else:
-        print(RED+"ERROR: " + WHITE + "Your operating system is not compatible.")
+        printError("Your operating system is not compatible with Live Session Mode.")
         exit(1)
 
     return webdriver.Chrome(executable_path= driverpath, options=opt, desired_capabilities=caps)
-     
+    
+def hideCodeKeys(action): # presses Ctrl + Shift + H
+    action.key_down(controlKey)
+    action.key_down(Keys.SHIFT)
+    action.key_down("H")
+    action.perform()
+    action.key_up("H")
+    action.key_up(Keys.SHIFT)
+    action.key_up(controlKey)                   
+    action.perform()
+
+def executeCode(action): # presses Ctrl + Shift + Enter
+    action.key_down(controlKey)
+    action.key_down(Keys.SHIFT)
+    action.key_down(Keys.ENTER)
+    action.perform()
+    action.key_up(Keys.ENTER)
+    action.key_up(Keys.SHIFT)
+    action.key_up(controlKey)                   
+    action.perform()
+
+def printLiveLoadingInfo(loadingText, loadingArray, loadingBackSpace):
+    print(loadingBackSpace, end="\r")
+    print(loadingText+loadingArray[random.randint(0,len(loadingArray)-1)], end="\r")
+
+def printLiveCodeStatus(pos):
+    if(pos==0):
+        print(BOLD+YELLOW+"----------------------------")
+        print("Loading new code in Hydra...")
+        print("----------------------------")
+        print('\033[2A', end="")
+    if(pos==1):
+        print('\033[1A', end="")
+        print(GREEN+"                               ")
+        print(GREEN+"                               ")
+        print(GREEN+"                               ", end="\r")
+        print('\033[2A', end="")      
+    if(pos==2):
+        print(BOLD+GREEN+"-------------")
+        print("Code loaded √")
+        print("-------------"+WHITE)
 
 def livePrinting(fullCode, textarea, action, area):
     waitTime=0.01
     loadingText="Loading new code in Hydra "
     loadingBackSpace="                              "
-    loadingArray=["⠼", "⠩", "⠡", "⠌", "⠴", "⠲", "⠢", "⠦", "⠍"]
+    if runningOnWindows:
+        loadingArray=["#", ".", "-", "/", "\\", "^", "+", "<", ">"] # windows friendly...
+    else:
+        loadingArray=["⠼", "⠩", "⠡", "⠌", "⠴", "⠲", "⠢", "⠦", "⠍"]
     done=False
     while not done:
         try:
-            print(loadingText+loadingArray[random.randint(0,len(loadingArray)-1)], end="\r")            
-            action.key_down(controlKey)
-            action.key_down(Keys.SHIFT)
-            action.key_down("H")
-            action.perform()
-            action.key_up("h")
-            action.key_up(Keys.SHIFT)
-            action.key_up(controlKey)                   
-            action.perform()
-            print(loadingBackSpace, end="\r")
-            print(loadingText+loadingArray[random.randint(0,len(loadingArray)-1)], end="\r")
-            area.click(); #Click on browser screen
-            #time.sleep(waitTime) 
-            print(loadingBackSpace, end="\r")
-            print(loadingText+loadingArray[random.randint(0,len(loadingArray)-1)], end="\r")
+            printLiveLoadingInfo(loadingText, loadingArray, loadingBackSpace)
+            if hideCode:
+                hideCodeKeys(action)
+            printLiveLoadingInfo(loadingText, loadingArray, loadingBackSpace)
+            area.click() #Click on browser screen
+            printLiveLoadingInfo(loadingText, loadingArray, loadingBackSpace)
             textarea.send_keys(controlKey + "a"); #Ctrl+a to select all code
-            #time.sleep(waitTime) 
-            print(loadingBackSpace, end="\r")
-            print(loadingText+loadingArray[random.randint(0,len(loadingArray)-1)], end="\r")
+            printLiveLoadingInfo(loadingText, loadingArray, loadingBackSpace)
             textarea.send_keys(fullCode) #writes new code overwriting old one
-            #time.sleep(waitTime) 
-            print(loadingBackSpace, end="\r")
-            print(loadingText+loadingArray[random.randint(0,len(loadingArray)-1)], end="\r")
-            action.key_down(controlKey) #ctrl + shift + enter to execute new code
-            action.key_down(Keys.SHIFT)
-            action.key_down(Keys.ENTER)
-            action.perform()
-            action.key_up(Keys.ENTER)
-            action.key_up(Keys.SHIFT)
-            action.key_up(controlKey)                   
-            action.perform()
-            print(loadingBackSpace, end="\r")
-            print(loadingText+loadingArray[random.randint(0,len(loadingArray)-1)], end="\r")
-            area.click(); #Click on browser screen
-            action.key_down(controlKey)
-            action.key_down(Keys.SHIFT)
-            action.key_down("h")
-            action.perform()
-            action.key_up("h")
-            action.key_up(Keys.SHIFT)
-            action.key_up(controlKey)                   
-            action.perform()
-
+            printLiveLoadingInfo(loadingText, loadingArray, loadingBackSpace)
+            executeCode(action)
+            printLiveLoadingInfo(loadingText, loadingArray, loadingBackSpace)
+            area.click()
+            if hideCode:
+                hideCodeKeys(action)
             done = True
         except selenium.common.exceptions.ElementNotInteractableException:
             print("Please click on Hydra text editor to write new code (in the web browser)")
@@ -250,11 +284,13 @@ def encodeText(fullCode):
     return fullCode
 
 def generateCode(hydra, functionsAmount): #This method will be refactored
+    terminalSize = os.get_terminal_size().columns
+    printBanner(terminalSize)
     fullCode=""
     staticBar = "----------------------------------------------------------------------------------------------"
     info="// Random Hydra code generated by HCG: https://github.com/alecominotti/hydracodegenerator/\n// @alecominotti\n"
     if args.live:
-        print(CYAN+BOLD+"LIVE SESSION MODE\n" + WHITE)
+        print(CYAN+BOLD+"LIVE SESSION MODE" + CYAN + " [BETA]\n" + WHITE)
     if args.function:
         print(WHITE+"Functions: " + str(functionsAmount) + " (Constant)")
     else:
@@ -297,7 +333,7 @@ def generateCode(hydra, functionsAmount): #This method will be refactored
             print()        
     if not args.ignore and (not args.use_all):
         print(DARKCYAN + " (Default)" + WHITE)
-    terminalSize = os.get_terminal_size().columns
+    
     bar=""
     for z in range(int(terminalSize)):
         bar+="-"
@@ -341,15 +377,13 @@ def main():
         textarea=""
         action=""
         area=""
-    while True:    
-        printBanner()
+    while True:
         functionsAmount= random.randint(minRandomFunctions, maxRandomFunctions)
         hydraCode = generateCode(hydra, functionsAmount)
         encodedHydraCode = encodeText(hydraCode)
         hydraFinalURL = hydraURL + encodedHydraCode
-        print(WHITE+"Select code and press " + GREEN + "Ctrl+Shift+C " + WHITE + "to copy it")
         print(WHITE+"Press " + GREEN + "Enter " + WHITE + "to generate new code")
-        print(WHITE+"Code was saved in '" + GREEN + "hydraCode.txt" + WHITE + "'")
+        print(WHITE+"Code was saved in '" + GREEN + txtfile + WHITE + "'")
         if not args.live:            
             print(WHITE+"\nOpen the following link to run Hydra with this code:")
             print(BLUE + hydraFinalURL + WHITE +  "\n")
@@ -357,36 +391,21 @@ def main():
         if args.live and firstTime:
             driver.get(hydraFinalURL)
             textarea = driver.find_elements(By.CSS_SELECTOR, '.CodeMirror textarea')[0]
-            area = driver.find_elements(By.ID, 'editor-container')[0]
+            area = driver.find_elements(By.CLASS_NAME, 'CodeMirror')[0]
             action = ActionChains(driver)
-            area.click(); #Click on browser screen
-            action.key_down(controlKey)
-            action.key_down(Keys.SHIFT)
-            action.key_down("h")
-            action.perform()
-            action.key_up("h")
-            action.key_up(Keys.SHIFT)
-            action.key_up(controlKey)                   
-            action.perform()
+            area.click()
+            if hideCode:
+                hideCodeKeys(action)
         if args.live and not firstTime:
             try:
-                print(BOLD+YELLOW+"----------------------------")
-                print("Loading new code in Hydra...")
-                print("----------------------------")
-                print('\033[2A', end="")
+                printLiveCodeStatus(0)
                 livePrinting(hydraCode, textarea, action, area)
-                print('\033[1A', end="")
-                print(GREEN+"                               ")
-                print(GREEN+"                               ")
-                print(GREEN+"                               ", end="\r")
-                print('\033[2A', end="")                    
+                printLiveCodeStatus(1)                
             except (selenium.common.exceptions.WebDriverException, selenium.common.exceptions.NoSuchWindowException):
-                print(RED+"ERROR: " + WHITE + "Web browser was closed.")
+                printError("Web browser was closed.")
                 exit(1)
         if args.live:       
-            print(BOLD+GREEN+"-------------")
-            print("Code loaded ✓")
-            print("-------------"+WHITE)
+            printLiveCodeStatus(2)
         if firstTime:
             firstTime = False
         if web:
